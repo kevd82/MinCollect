cdb656b24ab5f5244f3b4f55b2cee5f3f91ab053
Your Name <you@example.com>
2024-01-05 19:44:36 -0800

    added createNewMiniature to Miniature controller

--- server/controllers/miniature.contoller.js
+++ server/controllers/miniature.contoller.js
@@ -15,7 +15,23 @@ module.exports = {
                 console.log("findAllMiniatures has failed");
                 res.json({message: "Error in findAllMiniatures", error: err})
             })
-        }
+        },
+
+    createNewMiniature: (req, res)=>{
+        Miniature.create(req.body)
+            .then((newMiniature)=>{
+                console.log(newMiniature);
+                res.json(newMiniature)
+            })
+            .catch((err)=>{
+                console.log("createNewMiniature has failed");
+                res.status(400).json(err)
+            })
+    },
+
+    findOneMiniature: (req, res)=>{
+        
+    }
 
 
 }
