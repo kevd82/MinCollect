5da704b8a61a2e6c7e7a1b0ad2fc773a62b4ef15
Your Name <you@example.com>
2024-01-08 13:49:27 -0800

    added Miniature routes

--- server/routes/miniature.routes.js
+++ server/routes/miniature.routes.js
@@ -4,10 +4,9 @@ const MiniatureController = require("../controllers/miniature.contoller");
 
 module.exports= (app)=>{
     app.get("/api/miniatures", MiniatureController.findAllMiniatures);
-    
-
-
-
-
+    app.post("/api/miniatures", MiniatureController.createNewMiniature);
+    app.get("/api/miniatures/:id", MiniatureController.findOneMiniature);
+    app.delete("/api/miniatures/:id", MiniatureController.deleteOneMiniature);
+    app.put("/api/miniatures/:id", MiniatureController.updateMiniature);
 
 }
--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors({origin: "http://localhost:3000"}));
 
 require("./config/mongoose.config");
-// require("./routes/miniature.routes")(app);
+require("./routes/miniature.routes")(app);
 
 app.listen(8000, () => console.log("You are connected to port 8000"));
